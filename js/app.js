a605d7248fb2f8e3d51ba386f290615d9cb95c03
Ryder Moody <rydthemoodster@gmail.com>
2014-03-20 23:49:02 -0700

    Nice fade

--- js/app.js
+++ js/app.js
@@ -1,16 +1,8 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 
-
 $(document).foundation();
 
 $("#content").click(function() {
     $('#myModal').foundation('reveal', 'open');
 });
-
-$('#Logo').hide();
-
-$(document).ready( function() {
-  $('#Logo').fadeIn(4000);
-});
-
